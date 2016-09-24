73d56e51219cc2db435087354cf964b05230a5ba
Blake Plumb <plumbblake@gmail.com>
2016-09-24 15:26:12 -0600

    masonry done

--- masonry/main.js
+++ masonry/main.js
@@ -3,11 +3,23 @@
     var skip = 0;
     var request = new XMLHttpRequest();
     var data = {};
-    var section = document.querySelector('section');
+    var section = document.querySelector('#main-section');
+    var loadMore = document.querySelector('#load-more');
+    var template = document.querySelector('#template');
+    var templateImage = template.content.querySelector('img');
+    var templateHeading = template.content.querySelector('h3');
+    var templateContent = template.content.querySelector('.content');
+    var templateMeta = template.content.querySelector('.meta');
 
     var loadData = function(){
-        for(skip; skip < take; skip++){
-            
+        for(skip; skip < take && skip < data.tiles.length; skip++) {
+            var tile = data.tiles[skip];
+            templateImage.src = tile.image;
+            templateHeading.textContent = tile.heading;
+            templateContent.textContent = tile.content;
+            templateMeta.textContent = tile.meta;
+            var templateClone = document.importNode(template.content, true);
+            section.appendChild(templateClone);
         }
     };
 
@@ -19,5 +31,14 @@
         loadData();
       }
     };
+
     request.send();
+
+    loadMore.addEventListener('click', function(){
+        take += 5;
+        if(take >= data.tiles.length){
+            loadMore.style.display = 'none';
+        }
+        loadData();
+    });
 })();
